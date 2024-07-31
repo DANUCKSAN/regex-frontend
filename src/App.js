8c3bb1bc237038c3b9c43d486011519c94c52657
DANUCKSAN <sathiyarajdanucksan@gmail.com>
2024-07-31 17:26:33 +1000

    commit-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import FileUpload from './components/FileUpload';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Regex Pattern Matching and Replacement</h1>
+            </header>
+            <FileUpload />
+        </div>
+    );
 }
 
 export default App;
